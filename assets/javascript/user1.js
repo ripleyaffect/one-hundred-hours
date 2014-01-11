4840e53aad6771be930797e9afeeb4268db8878a
ripleyaffect <t.ripley.peterson@gmail.com>
2014-01-11 16:25:50 -0500

    Add progress bars, styling, and error checking

--- assets/javascript/user1.js
+++ assets/javascript/user1.js
@@ -2,10 +2,11 @@ angular.module('user1', [])
   .controller('UserController', function() {
     this.username = "Stranger";
     this.projectName= "";
+    this.projectDescription = "";
     this.timeLimit = 20;
     this.hours = 0.0
     this.minutes = 0.0
-    this.projects = [{name: "Example Project", started: 16080.78, hours: 5.0, timeLimit: 15, visible: false}]
+    this.projects = [{name: "Example Project", description: "An example Project. Feel free to delete it.", started: 16080.78, hours: 5.0, timeLimit: 15, visible: false}]
     this.welcome = function welcome() {
       if(this.username === undefined) {
         this.username = "";
@@ -15,6 +16,10 @@ angular.module('user1', [])
 
     this.getProjectName = function getProjectName(project) {
       return project.name;
+    };
+
+    this.getProjectDescription = function getProjectDescription(project) {
+      return project.description;
     };
 
     this.createProject = function createProject() {
@@ -27,14 +32,13 @@ angular.module('user1', [])
         return;
       };
       date = (new Date().getTime()) / (1000 * 60 * 60 * 24);
-      this.projects.push({name: this.projectName, started: date, hours: 0.0, timeLimit: this.timeLimit, visible: false});
+      this.projects.push({name: this.projectName, description: this.projectDescription, started: date, hours: 0.0, timeLimit: this.timeLimit, visible: false});
+      this.projectName = "";
+      this.projectDescription = "";
     };
 
     this.percentComplete = function percentComplete(project) {
       percent = (project.hours).toFixed(2);
-      if(percent > 100) {
-        percent = 100;
-      }
       return percent
     };
 
@@ -47,7 +51,22 @@ angular.module('user1', [])
       return percent
     };
 
+    this.setProgressWidth = function setProgressWidth(project) {
+      element = "width:" + project.hours + "%";
+      return element;
+    }
+
+    this.setTimeWidth = function setTimeWidth(project) {
+      element = "width:" + this.percentTimeElapsed(project) + "%";
+      return element;
+    }
+
     this.toggleEditProject = function toggleEditProject(project) {
+      this.projects.forEach(function(entry) {
+        if(entry != project) {
+          entry.visible = false;
+        }
+      });
       project.visible = !project.visible;
     };
 
@@ -55,7 +74,16 @@ angular.module('user1', [])
       if(isNaN(this.hours + this.minutes)) {
         return;
       };
+      if(this.hours < 0 || this.minutes < 0) {
+        return;
+      }
       project.hours = project.hours + (this.hours + (this.minutes / 60));
+      if(project.hours < 0) {
+        project.hours = 0;
+      }
+      if(project.hours > 100) {
+        project.hours = 100;
+      }
     };
 
     this.deleteProject = function deleteProject(project) {
@@ -64,5 +92,6 @@ angular.module('user1', [])
         this.projects.splice(this.projects.indexOf(project), 1);
       }
     };
+
 
   });
